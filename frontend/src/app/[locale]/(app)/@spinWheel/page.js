7652a41f757554bd310670022aa9a94abb59c25a
tpervadorit <trunal@pervadorit.com>
2025-10-07 11:13:34 +0530

    @Spinwheel issue solve #2

--- frontend/src/app/[locale]/(app)/@spinWheel/page.js
+++ frontend/src/app/[locale]/(app)/@spinWheel/page.js
@@ -1,4 +1,12 @@
-import SpinWheel from '@/components/SpinWheel/components';
-export default function Page() {
-  return <SpinWheel />;
+// import SpinWheel from '@/components/SpinWheel/components';
+// export default SpinWheel;
+
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div>@SpinWheel##</div>
+  )
 }
+
+export default Page
