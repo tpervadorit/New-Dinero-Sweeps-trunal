34c4f3e342e867d96f29d8d1cb022de8ea78977e
tpervadorit <trunal@pervadorit.com>
2025-10-07 11:23:16 +0530

    @SpinWheel #3

--- frontend/src/app/[locale]/(app)/@spinWheel/page.js
+++ frontend/src/app/[locale]/(app)/@spinWheel/page.js
@@ -1,12 +1,2 @@
-// import SpinWheel from '@/components/SpinWheel/components';
-// export default SpinWheel;
-
-import React from 'react'
-
-const Page = () => {
-  return (
-    <div>@SpinWheel##</div>
-  )
-}
-
-export default Page
+import SpinWheel from '@/components/SpinWheel/components';
+export default SpinWheel;
